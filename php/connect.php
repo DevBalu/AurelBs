5ef51e788a99ea418e633e6f67ceee15ca62b4d2
DevBalu <ghenov.andrei@gmail.com>
2017-06-26 17:16:00 +0300

    add

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,6 @@
 <?php
-	$con = new mysqli("localhost", "root", "", "topaza");
+	// $con = new mysqli("localhost", "root", "", "topaza");
+	$con = new mysqli("213.171.200.94", "viorel", "5Hp242424", "topaza");
 
 	if(mysqli_connect_errno()){
 		printf("Connect failed: %s\n", mysqli_connect_error());
